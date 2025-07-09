cb497eea308f48762751210efe37e44827bb66af
gxickx <mgrokoe@gmail.com>
2025-07-09 19:05:10 -0300

    Se configuró la clase CicloCine para la persistencia con JPA, además de agregar que películas sean una lista dentro de la clase.

--- src/main/java/modelos/CicloCine.java
+++ src/main/java/modelos/CicloCine.java
@@ -1,14 +1,29 @@
 package modelos;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cicloCine")
 public class CicloCine extends Evento {
-    private Pelicula[] peliculas;
+
+    @Column(nullable = false)
     private boolean tieneCharlas;
+
+    public CicloCine() {
+        super();
+    }
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "cicloCine_id")
+    private List<Pelicula> peliculas = new ArrayList<>();
+
 
     public CicloCine(String nombre, Date fechaInicio, int duracion, EstadoEvento estado, boolean requiereInscripcion, boolean tieneCupo, int cupoMaximo, boolean tieneCharlas) {
         super(nombre, fechaInicio, duracion, estado, requiereInscripcion, tieneCupo, cupoMaximo);
-        
         this.tieneCharlas = tieneCharlas;
     }
 
@@ -16,9 +31,24 @@ public class CicloCine extends Evento {
         return tieneCharlas;
     }
 
+    public void setTieneCharlas(boolean tieneCharlas) {
+        this.tieneCharlas = tieneCharlas;
+    }
+
     public void agregarPelicula(Pelicula p){
         if (p == null) {
             throw new IllegalArgumentException("La película no puede ser nula.");
+        } else {
+            peliculas.add(p);
         }
     }
+
+    public List<Pelicula> getPeliculas() {
+        return peliculas;
+    }
+
+    public void setPeliculas(List<Pelicula> peliculas) {
+        this.peliculas = peliculas;
+    }
+
 }
