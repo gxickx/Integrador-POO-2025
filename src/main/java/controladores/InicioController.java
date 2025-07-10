9339974ef6945fd31d934c693c7aad0e8492e8ef
Nathelin <menachonatalian@gmail.com>
2025-07-10 11:11:51 -0300

    Creación pantalla básica de inicio, faltan las acciones onClick para ir a las distintas secciones, y settear como página de inicio

---
+++ src/main/java/controladores/InicioController.java
@@ -0,0 +1,33 @@
+package controladores;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
+
+public class InicioController {
+
+    @FXML
+    private Menu btnIrEventos;
+
+    @FXML
+    private Menu btnIrParticipacion;
+
+    @FXML
+    private Menu btnIrPersonas;
+
+    @FXML
+    void onClickIrEventos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickIrParticipacion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickIrPersonas(ActionEvent event) {
+
+    }
+
+}
